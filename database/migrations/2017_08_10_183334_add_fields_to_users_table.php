73527aa16bdbb5b507ca1d47a5784246ad143156
Facundo Encina <fencina@bondacom.com>
2017-08-10 15:09:44 -0300

    Add roles table. Add fields on users table.

---
+++ database/migrations/2017_08_10_183334_add_fields_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('last_name')->after('name');
+            $table->string('file_number')->after('last_name')->unique();
+            $table->unsignedInteger('role_id')->after('password');
+
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('last_name');
+            $table->dropColumn('file_number');
+
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
+        });
+    }
+}
