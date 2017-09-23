a53899fbcf04aff3ff6f8e1b170ece7d1657732b
Facundo Encina <fencina@bondacom.com>
2017-09-23 15:45:03 -0300

    Update API login method

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -2,32 +2,29 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\ClientLoginRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function login(ClientLoginRequest $request)
     {
 
         $client = new \GuzzleHttp\Client();
 
-        try {
-            $response = $client->request('POST', url('/oauth/token'), [
-                'json' => [
-                    "grant_type" => "password",
-                    "client_id" => 2,
-                    "client_secret" => "XOry4JuJewq9pOaA7us4DQsoBNxgiDDyzxjlRgRO",
-                    "username" => $request->input('email'),
-                    "password" => $request->input('password')
-                ]
-            ]);
+        $response = $client->request('POST', url('/oauth/token'), [
+            'json' => [
+                "grant_type" => "password",
+                "client_id" => 2,
+                "client_secret" => "XOry4JuJewq9pOaA7us4DQsoBNxgiDDyzxjlRgRO",
+                "username" => $request->input('email'),
+                "password" => $request->input('password')
+            ]
+        ]);
 
-            $response = json_decode($response->getBody()->getContents());
+        $response = json_decode($response->getBody()->getContents());
 
-            return response()->json($response, 200);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
+        return response()->json($response, 200);
     }
 }
