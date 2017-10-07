8d12da13c445117bf64b26662f0cfad1b9f86e27
Facundo Encina <fencina@bondacom.com>
2017-10-07 17:23:35 -0300

    Create groups and client_group table

---
+++ database/migrations/2017_10_07_043659_create_client_group_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_group', function (Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('client_id');
+            $table->unsignedInteger('group_id');
+            $table->boolean('owner')->default(false);
+
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('group_id')->references('id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_group');
+    }
+}
