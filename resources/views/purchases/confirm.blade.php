8260b34b889374f40d73d2f576be775aee30dfab
Facundo Encina <fencina@bondacom.com>
2017-11-04 22:52:16 -0300

    Add endpoint to associate a purchase to a group

--- resources/views/purchases/confirm.blade.php
+++ resources/views/purchases/confirm.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('title', 'Com')
+@section('title', 'Compras')
 
 @section('content')
 
@@ -8,5 +8,15 @@
         {!! QrCode::size(100)->generate( $purchase->id ) !!}
         <p>¡Confirmá tu compra!</p>
     </div>
+
+
+    <script>
+        $(document).ready( function() {
+            Echo.private('purchase-associated')
+                .listen('PurchaseAssociated', (e) => {
+                window.location.replace("{{ url('purchases') }}");
+        })
+        })
+    </script>
 
 @endsection
