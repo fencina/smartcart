27555ea6147a205172a87d06ddcfa8546c693fd8
rojascristian <rojas.r.cristian@gmail.com>
2017-11-08 23:22:37 -0300

    Fix: se cambia la forma de importar los assets: desde apache server no los importaba bien

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,12 +5,14 @@
 
         <title>@yield('title')</title>
 
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
 
         <!-- Font Awesome CDN -->
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-        <script src="{{ mix('js/app.js') }}"></script>
+        <!--<script src="{{ mix('js/app.js') }}"></script>-->
+        <script src="{{ asset('js/app.js') }}"></script>
+
 
     </head>
 
