f7631fc236a8318637eeff6409f71edc43047653
Facundo Encina <fencina@bondacom.com>
2017-11-09 22:00:37 -0300

    Store final products when purchase is confirmed

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use App\Purchase;
 use App\Status;
 use Illuminate\Http\Request;
@@ -17,7 +18,22 @@ class PurchaseController extends Controller
 
     public function confirm(Request $request, Purchase $purchase)
     {
-        $purchase->status()->associate(Status::confirmed())->save();
+        $purchase->status()->associate(Status::confirmed());
+
+        $finalProducts = collect(json_decode($request->input('finalProducts')));
+        $products = Product::whereIn('id', $finalProducts->pluck('id'))->get();
+
+        $purchase->amounts = $products->sum( function ($product) use ($finalProducts) {
+            return $product->price * $finalProducts->where('id', $product->id)->first()->count;
+        });
+
+        $purchase->save();
+
+        $products = $finalProducts->mapWithKeys(function ($product) {
+            return [$product->id => ['count' => $product->count]];
+        })->toArray();
+
+        $purchase->products()->sync($products);
 
         return view('purchases.confirm', compact('purchase'));
     }
