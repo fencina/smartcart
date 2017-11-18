5fac26c7c0afabf44dcd4e32ed02eb098511ec71
Facundo Encina <fencina@bondacom.com>
2017-11-18 17:56:44 -0300

    Improve ionic push notification service

--- app/Services/IonicPushNotificationService.php
+++ app/Services/IonicPushNotificationService.php
@@ -24,7 +24,37 @@ class IonicPushNotificationService
         ];
     }
 
-    public function broadcast($message, $title = null)
+    /**
+     * Send a notification to all clients
+     *
+     * @param $message
+     * @param null $title
+     */
+    public function broadcast($message, $title = null, $payload = null)
+    {
+        $this->send($this->buildNotification($message, $title), true, null, $payload);
+    }
+
+    /**
+     * Send notifications to a client segment
+     *
+     * @param $tokens
+     * @param $message
+     * @param null $title
+     */
+    public function sendTo($tokens, $message, $title = null, $payload = null)
+    {
+        $this->send($this->buildNotification($message, $title), false, $tokens, $payload);
+    }
+
+    /**
+     * Build notification array with message and title (optional)
+     *
+     * @param $message
+     * @param null $title
+     * @return array
+     */
+    protected function buildNotification($message, $title = null)
     {
         $notification['message'] = $message;
 
@@ -32,13 +62,36 @@ class IonicPushNotificationService
             $notification['title'] = $title;
         }
 
+        return $notification;
+    }
+
+    /**
+     * Send push notification through ionic API
+     *
+     * @param $notification
+     * @param bool $toAll
+     * @param null $tokens
+     * @param null $payload
+     */
+    protected function send($notification, $toAll = false, $tokens = null, $payload = null)
+    {
+        $requestBody = [
+            "send_to_all" => $toAll,
+            "profile" => "dev",
+            "notification" => $notification
+        ];
+
+        if ($tokens) {
+            $requestBody['tokens'] = $tokens;
+        }
+
+        if ($payload) {
+            $requestBody['payload'] = $payload;
+        }
+
         $this->guzzleClient->request('POST', url('https://api.ionic.io/push/notifications'), [
             'headers' => $this->requestHeaders,
-            'json' => [
-                "send_to_all" => true,
-                "profile" => "dev",
-                "notification" => $notification
-            ]
+            'json' => $requestBody
         ]);
     }
 }
